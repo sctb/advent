88943d5bf9faa8d9c2c65206d0cd7a06fd2b9937
Scott Bell <sctb@me.com>
2025-01-01 11:11:29 -0700

    Day 7 part 1

--- day-7.el
+++ day-7.el
@@ -23,6 +23,34 @@
 	(setq max (max max n))))
     max))
 
+(defun wholep (n)
+  (zerop (mod n 1)))
+
+(defun run-search (value numbers)
+  (if-let* ((rest (cdr numbers)))
+      (let* ((n (car numbers))
+	     (delta (- value n))
+	     (factor (/ (float value) n)))
+	(when (> delta 0)
+	  (run-search delta rest))
+	(when (and (>= factor 1) (wholep factor))
+	  (run-search (truncate factor) rest)))
+    (when (= value (car numbers))
+      (throw :true t))))
+
+(defun test-equation (e)
+  (let ((value (car e))
+	;; this approach inverts the operators to recursively search
+	;; for matches, so we also need to reverse the application
+	;; order
+	(numbers (reverse (cdr e))))
+    (when (catch :true (run-search value numbers))
+      value)))
+
 (defun puzzle-7a ()
-  (let ((equations (read-equations "data/example-7.txt")))
-    (max-size equations)))
+  (let ((equations (read-equations "data/input-7.txt"))
+	(sum 0))
+    (dolist (e equations)
+      (when-let* ((value (test-equation e)))
+	(setq sum (+ sum value))))
+    sum))
