c9801f23ca1603c06c26ba288778ee68bc5bf1fb
Scott Bell <sctb@me.com>
2025-01-01 13:51:42 -0700

    Day 7 part 2

--- day-7.el
+++ day-7.el
@@ -54,3 +54,40 @@
       (when-let* ((value (test-equation e)))
 	(setq sum (+ sum value))))
     sum))
+
+(defun run-search-2 (value numbers)
+  (if-let* ((rest (cdr numbers)))
+      (let* ((n (car numbers))
+	     (delta (- value n))
+	     (factor (/ (float value) n)))
+	(when (> delta 0)
+	  (run-search-2 delta rest))
+	(when (and (>= factor 1) (wholep factor))
+	  (run-search-2 (truncate factor) rest))
+	(let ((a (number-to-string value))
+	      (b (number-to-string n)))
+	  ;; to invert the concatenation operator we strip the value
+	  ;; of its suffix, if possible
+	  (when (string-suffix-p b a)
+	    (let* ((prefix (substring a 0 (- (length b))))
+		   (value (string-to-number prefix)))
+	      (run-search-2 value rest)))))
+    (when (= value (car numbers))
+      (throw :true t))))
+
+(defun test-equation-2 (e)
+  (let ((value (car e))
+	;; this approach inverts the operators to recursively search
+	;; for matches, so we also need to reverse the application
+	;; order
+	(numbers (reverse (cdr e))))
+    (when (catch :true (run-search-2 value numbers))
+      value)))
+
+(defun puzzle-7b ()
+  (let ((equations (read-equations "data/input-7.txt"))
+	(sum 0))
+    (dolist (e equations)
+      (when-let* ((value (test-equation-2 e)))
+	(setq sum (+ sum value))))
+    sum))
