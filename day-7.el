1eb55083c55e11713e3bd5b0bcba017171be5150
Scott Bell <sctb@me.com>
2025-01-01 08:43:19 -0700

    Day 7 initial utilities

--- day-7.el
+++ day-7.el
@@ -1,3 +1,28 @@
 ;; -*- lexical-binding: t -*-
 
+(defun sread (string)
+  (car (read-from-string string)))
 
+(defun read-equations (file)
+  (let ((equations nil))
+    (with-temp-buffer
+      (insert-file-contents file)
+      (while (not (eobp))
+	(let* ((line (buffer-substring (point) (line-end-position)))
+	       (split (split-string line ":"))
+	       (value (sread (car split)))
+	       (numbers (sread (format "(%s)" (cadr split)))))
+	  (push (cons value numbers) equations))
+	(forward-line)))
+    (nreverse equations)))
+
+(defun max-size (equations)
+  (let ((max 0))
+    (dolist (e equations)
+      (let ((n (length (cdr e))))
+	(setq max (max max n))))
+    max))
+
+(defun puzzle-7a ()
+  (let ((equations (read-equations "data/example-7.txt")))
+    (max-size equations)))
