afab861dbcc1b8d5db58acdc3efd11cf9b7c2e2b
Scott Bell <sctb@me.com>
2025-01-04 14:19:59 -0700

    Day 11 part 1

--- day-11.el
+++ day-11.el
@@ -1,3 +1,35 @@
 ;; -*- lexical-binding: t -*-
 
+(defun read-all ()
+  (let ((forms nil))
+    (while-let ((form (ignore-errors (read (current-buffer)))))
+      (push form forms))
+    (nreverse forms)))
 
+(defun read-stones (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (read-all)))
+
+(defun evenp (n)
+  (and (> n 0) (= (% n 2) 0)))
+
+(defun change-stone (n)
+  (if (eq n 0)
+      '(1)
+    (let* ((digits (number-to-string n))
+	   (len (length digits)))
+      (if (evenp len)
+	  (let ((i (/ len 2)))
+	    (list (string-to-number (substring digits 0 i))
+		  (string-to-number (substring digits i))))
+	(list (* n 2024))))))
+
+(defun blink (stones)
+  (seq-mapcat #'change-stone stones))
+
+(defun puzzle-11a ()
+  (let ((stones (read-stones "data/input-11.txt")))
+    (dotimes (_ 25)
+      (setq stones (blink stones)))
+    (length stones)))
