a353b46adb3f2142ed8a4e8f753f3eefb7fd7e60
Scott Bell <sctb@me.com>
2025-03-31 10:50:07 -0600

    WIP on Day 7

--- 2023/day-7.el
+++ 2023/day-7.el
@@ -15,5 +15,46 @@
 	(forward-line))
       (nreverse hands))))
 
+(defun hand-type (hand)
+  (let ((cards nil))
+    (seq-do (lambda (c)
+	      (let ((n (alist-get c cards 0)))
+		(setf (alist-get c cards) (1+ n))))
+	    hand)
+    (let* ((counts (mapcar #'cdr cards))
+	   (lead (car (sort counts :reverse t))))
+      (pcase (length cards)
+	(5 1)				; distinct
+	(4 2)				; one pair
+	(3 (if (eq lead 3) 4 3))	; three of a kind or two pair
+	(2 (if (eq lead 4) 5 4))	; four of a kind or full house
+	(1 6)))))			; five of a kind
+
+(defun hand-score (hand)
+  (let ((order "23456789TJQKA")
+	(high (seq-reverse hand))
+	(score 0))
+    (dotimes (i (length hand))
+      (let* ((c (elt high i))
+	     (factor (expt 10 i))
+	     (rank (seq-position order c)))
+	(setq score (+ score (* factor rank)))))
+    (let* ((i (1+ (length hand)))
+	   (factor (expt 10 i))
+	   (type (hand-type hand)))
+      (setq score (+ score (* factor type))))
+    score))
+
+(defun sort-hands (pairs)
+  (sort pairs :key (lambda (pair) (hand-score (car pair)))))
+
 (defun puzzle-7a ()
-  (read-hands "data/example-7.txt"))
+  (let* ((pairs (read-hands "data/example-7.txt"))
+	 (sorted (sort-hands pairs))
+	 (rank 1)
+	 (winnings 0))
+    (dolist (pair sorted)
+      (let ((bid (cdr pair)))
+	(setq winnings (+ winnings (* bid rank))))
+      (setq rank (1+ rank)))
+    winnings))
